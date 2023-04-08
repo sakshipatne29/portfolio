9874b94fb5347494483dad49af085d32184cab45
Sakshi Patne <sakshipatne206@gmail.com>
2023-04-09 03:33:27 +0530

    header details added

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./header.css";
+
+const CTA = () => {
+  return (
+    <div className="cta">
+      <a href="#contact" className="btn">
+        Download CV
+      </a>
+      <a href="#contact" className="btn btn-primary">
+        Let's Talk
+      </a>
+    </div>
+  );
+};
+
+export default CTA;
