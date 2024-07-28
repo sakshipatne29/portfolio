a00ae456570d6221921f6d8863cc943768ad0a29
Sakshi Patne <sakshipatne206@gmail.com>
2024-07-29 02:11:28 +0530

    pushing all changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/header/Header";
+import Experience from "./components/experience/Experience";
+import Navbar from "./components/navbar/Navbar";
+import About from "./components/about/About";
+import Education from "./components/education/Education";
+import Projects from "./components/projects/Projects";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Header />
+      <About />
+      <Education />
+      <Experience />
+      <Projects />
+    </>
   );
-}
+};
 
 export default App;
